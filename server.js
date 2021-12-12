680f256fabb1d2c7c83b44c5daf14042081ebdfb
Kyle Lux <kyleslux86@gmail.com>
2021-12-12 16:41:44 -0500

    removed logger references

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const { urlencoded } = require('express');
 const express = require('express');
-const logger = require('logger');
+//const logger = require('logger');
 const mongoose = require('mongoose');
 const router = require('router');
 
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-app.use(logger('dev'));
+//app.use(logger('dev'));
 
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
